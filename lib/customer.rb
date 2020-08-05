fec4a7088f743912248eda111e10f3ee50a562d4
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-08-05 01:58:47 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,18 @@
 class Customer
-  
+  attr_accessor :waiter, :customer, :total, :tip
+
+  @@all = []
+
+  def initialize(waiter, customer, total, tip=0)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end 
 end
