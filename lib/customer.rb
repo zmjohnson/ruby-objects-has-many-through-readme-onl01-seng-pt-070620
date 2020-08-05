9494b75b49100286e812da1f4c2ed6c8f295cbd2
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-08-05 02:00:11 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,18 +1,50 @@
 class Customer
-  attr_accessor :waiter, :customer, :total, :tip
+  aattr_accessor :name, :age
 
   @@all = []
 
-  def initialize(waiter, customer, total, tip=0)
-    @waiter = waiter
-    @customer = customer
-    @total = total
-    @tip = tip
+  def initialize(name, age)
+    @name = name
+    @age = age
     @@all << self
   end
 
   def self.all
     @@all
   end
-end 
+
+ 
+  def meals
+    Meal.all.select do |meal|
+      meal.customer == self
+    end
+  end
+
+
+  def waiters
+    meals.map do |meal|
+      meal.waiter
+    end
+  end
+
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def new_meal_20_percent(waiter, total)
+    tip = total * 0.2
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def self.oldest_customer 
+    oldest_age = 0
+    oldest_customer = nil
+    self.all.each do |customer|
+      if customer.age > oldest_age
+        oldest_age = customer.age
+        oldest_customer = customer
+      end
+    end
+    oldest_customer
+  end
 end
